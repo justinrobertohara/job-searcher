318e3a88d18b1354890ee89449a61b2d35406bc9
Justin O'Hara <justinrobertohara@gmail.com>
2020-05-14 19:52:40 -0700

    changed Results to functional component

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,20 +1,8 @@
 import React from 'react';
 import Result from './Result.jsx';
 
-export default class Results extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    let listings = this.props.results;
-    return (
-      <div>
-        {listings.map((listing, key) => {
-          return <Result key={key} listing={listing} />;
-        })}
-      </div>
-    );
-  }
+export default function Results(props) {
+  return props.results.map((listing, key) => {
+    return <Result key={key} listing={listing} />;
+  });
 }
