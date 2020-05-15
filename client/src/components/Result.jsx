80124978d53f8bf72955e440eecb36ea044fc36b
Justin O'Hara <justinrobertohara@gmail.com>
2020-05-14 20:04:20 -0700

    added cards to result functional component

--- client/src/components/Result.jsx
+++ client/src/components/Result.jsx
@@ -1,5 +1,16 @@
 var React = require('react');
 
 export default function Result(props) {
-  return <p>{props.listing.title}</p>;
+  return (
+    <div>
+      <div className="card">
+        <div className="card-divider">{props.listing.title}</div>
+        <img src={props.listing.company_logo} />
+        <div className="card-section">
+          <h4>{props.listing.location}</h4>
+          <p>{props.listing.description}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
