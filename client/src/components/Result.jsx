ede4b83bf272278efdc497b9d41f404e29420423
Justin O'Hara <justinrobertohara@gmail.com>
2020-05-18 13:23:20 -0700

    implemented html parser on Result component

--- client/src/components/Result.jsx
+++ client/src/components/Result.jsx
@@ -1,14 +1,24 @@
 var React = require('react');
+import ReactHtmlParser from 'react-html-parser';
 
 export default function Result(props) {
   return (
     <div>
       <div className="card">
-        <div className="card-divider">{props.listing.title}</div>
-        <img src={props.listing.company_logo} />
+        <div className="card-divider">
+          {props.listing.type} Position {props.listing.title} for{' '}
+          {props.listing.company}
+        </div>
+        <img
+          src={props.listing.company_logo}
+          width={300}
+          height={150}
+          mode="fit"
+        />
         <div className="card-section">
           <h4>{props.listing.location}</h4>
-          <p>{props.listing.description}</p>
+          <p>{ReactHtmlParser(props.listing.how_to_apply)}</p>
+          <p>{ReactHtmlParser(props.listing.description)}</p>
         </div>
       </div>
     </div>
