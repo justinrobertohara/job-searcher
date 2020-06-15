394120d6c7b4f0f640c91d3489fb2a2b5abbad39
Justin O'Hara <justinrobertohara@gmail.com>
2020-06-15 00:07:32 -0700

    docker server port

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
+// const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const request = require('request');
 var cors = require('cors');
